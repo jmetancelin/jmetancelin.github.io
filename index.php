0e9ad2fe1fb110cc7992207c001f422713d30071
JM Etancelin <Jean-Matthieu.Etancelin@imag.fr>
2016-03-22 22:13:12 +0100

    update

--- index.php
+++ index.php
@@ -16,17 +16,18 @@
   <body>
     <div class="wrapper">
       <header>
-        <h1 class="header">JM Etancelin</h1>
-        <h2 class="header">PhD, Engineer</h2>
-        <p class="header">Research Engineer <br>
-	  ROMEO HPC Center<br>
-	  GPU Application Lab <br>
-	  Reims Champagne-Ardenne University <br> <br>
-	  UFR Sciences Exactes et Naturelles <br>
-	  Moulin de la Housse - BP 1039 <br>
-	  51687 Reims Cedex 2 <br>
-	  <a href="mailto:jean-matthieu.etancelin@univ-reims.fr">jean-matthieu.etancelin@univ-reims.fr</a> <br>
-	  +33(0) 3 26 91 89 09</p>
+	<?php include('infos.php'); ?>
+        <!-- <h1 class="header">JM Etancelin</h1> -->
+        <!-- <h2 class="header">PhD, Engineer</h2> -->
+        <!-- <p class="header">Research Engineer <br> -->
+	<!--   ROMEO HPC Center<br> -->
+	<!--   GPU Application Lab <br> -->
+	<!--   Reims Champagne-Ardenne University <br> <br> -->
+	<!--   UFR Sciences Exactes et Naturelles <br> -->
+	<!--   Moulin de la Housse - BP 1039 <br> -->
+	<!--   51687 Reims Cedex 2 <br> -->
+	<!--   <a href="mailto:jean-matthieu.etancelin@univ-reims.fr">jean-matthieu.etancelin@univ-reims.fr</a> <br> -->
+	<!--   +33(0) 3 26 91 89 09</p> -->
 
 
 	  <br>
